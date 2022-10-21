9ee5c993035ad773cfdd5b244ab384d6d62b3cec
Joseph A <joseph_anderson11@yahoo.com>
2022-10-21 14:23:47 -0400

    fix associations

--- models/index.js
+++ models/index.js
@@ -4,14 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+});
+
 // Products belongsTo Category
-Product.belongsTo(Category);
-// Categories have many Products
-Category.hasMany(Product);
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
+
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: ProductTag});
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: ProductTag});
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+});
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+// const { Category, Product } = require('../../models');
+const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const test = await Category.findAll({
+      // include: [{model: Product}]
+    });
+    res.status(200).json(test);
+  } catch(err){
+    res.status(500).json(err);
+  }
+  
 });
 
 router.get('/:id', (req, res) => {
