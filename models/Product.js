b4e570bf64ab3df502e542dbb166c116819ba0d9
Joseph A <joseph_anderson11@yahoo.com>
2022-10-21 09:41:33 -0400

    product model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTERGER,
+      defaultValue: 10,
+      allowNull: false,
+      validate: {
+        isInt: true
+      }     
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
